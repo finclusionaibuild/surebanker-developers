461ac9c37e1a74c44890f331721f27d0a145362d
finclusionaibuild <finclusionAIbuild@gmail.com>
2025-09-04 05:39:22 +0100

    Remove selected developer name div

--- src/screens/DeveloperDashboard/DeveloperDashboard.tsx
+++ src/screens/DeveloperDashboard/DeveloperDashboard.tsx
@@ -2062,7 +2062,6 @@ data = response.json()`
                 
                 <div className="flex items-center gap-3">
                   <div className="text-right">
-                    <div className="text-sm font-medium text-gray-900">Alex Developer</div>
                     <div className="text-xs text-gray-600">Developer Account</div>
                   </div>
                   <ProfileDropdown
